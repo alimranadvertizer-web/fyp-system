5dffb7cf38f701f148f3989fea8098346c2b53b3
M Zulqarnain <alimranadvertizer@gmail.com>
2026-05-20 09:58:04 +0500

    Revert "Added main body"
    This reverts commit fc0695e4bcf12a187ed50c7ffefd8a277c7639c9.

--- app.java
+++ app.java
@@ -1,11 +1,7 @@
 class app{
   //main body
- public static void main(String[] args) {
   int a = 10;
   int b = 10;
   int result = a + b;
   System.out.println(result);
-
-  // changes by 2nd member
- }
 }
